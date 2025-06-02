68b90b47d647a4eab17499dc9a9d7be1b267cff6
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-02 23:11:14 +0530

    Update Employee Management and Payroll Reporting System.py

--- Employee Management and Payroll Reporting System.py
+++ Employee Management and Payroll Reporting System.py
@@ -39,6 +39,11 @@ try:
         values
         ('IT'), ('HR'), ('Finance'), ('Marketing'), ('Operations');
         """)
+    cur.execute("select * from departments;")
+    print ("The generated table 'departments' is : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()    
     
     ## creating the table for employees
     cur.execute("""
@@ -63,6 +68,12 @@ try:
         ('Suman', 'Patel', 'Chennai', 'suman.patel@example.com', 3, '2018-07-30', 60000.00),
         ('Kavita', 'Rao', 'Pune', 'kavita.rao@example.com', 2, '2020-11-10', 47000.00);""")
     
+    cur.execute("select * from employees;")
+    print ("The generated table 'employees' is : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()  
+    
     ## creating the table for projects
     cur.execute("""
         create table projects(
@@ -80,6 +91,12 @@ try:
         ('Budget Planning', 3),
         ('Marketing Blitz', 4);""")
     
+    cur.execute("select * from projects;")
+    print ("The generated table 'projects' is : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()  
+    
     ## creating the table for employee_project
     cur.execute("""
         create table employee_project (
@@ -95,6 +112,12 @@ try:
         values
         (1,1), (2,2), (3,1), (4,3), (5,2);""")
     
+    cur.execute("select * from employee_project;")
+    print ("The generated table 'employee_project' is : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()  
+    
 
     ## performing queries
 
@@ -103,6 +126,11 @@ try:
         insert into employees (fname, lname, city, email, dept_id, joining_date, salary)
         values 
         ('Rohan','Mehra', 'Bengaluru', 'rohan.mehra@example.com', 2, current_date, 52000.00);""")
+    cur.execute("select * from employees;")
+    print ("Employees table after adding the new employee : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()  
 
     # 2. Updating the city of an Employee
     cur.execute("""
@@ -111,6 +139,10 @@ try:
         where fname = 'Rohan';
     """)
     cur.execute("select * from employees;")
+    print ("Changing the city to Jaipur : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 3. Deleting employee where fname = 'Suman'
     cur.execute("""
@@ -121,53 +153,88 @@ try:
         delete from employees 
         where emp_id = 4;
     """)
+    cur.execute("select * from employees;")
+    print ("Generated table after deleting employee 'Suman': ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 4. Showing employees where city contains 'i'
-
     cur.execute("""
         select * from employees
         where city like '%i%';
     """)
-    x = cur.fetchall()
-    print (x)
+    print ("Employees where the city name contains 'i' in between : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 5. Employees with salary between 45k and 50k
     cur.execute("""
         select * from employees 
         where salary between 45000 and 50000;
     """)
+    print ("Employees having salaries between 45k and 50k : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 6. Departments in alphabetical order
     cur.execute("""
         select * from departments 
         order by dept_name;
     """)
+    print ("Arranging departments in alphabetical order : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 7. Distinct city names from employees
     cur.execute("""
         select distinct city from employees;
     """)
+    print ("Distinct city names from the table 'employees' : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 8. Average salary of all employees
     cur.execute("""
         select avg(salary) as average_salary from employees;
     """)
+    print ("Average salary of all employees : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 9. Uppercasing fnames of all the employees
     cur.execute("""
         select upper(fname) as upper_fname from employees;
     """)
+    print ("Uppercasing fnames of all the employees : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 10. Extracting domain from the emails
     cur.execute("""
         select email, substring(email from position('@' in email)+1) as domain from employees;
     """)
+    print ("Extracting domain from the emails : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 11. Adding column 'Gender' to employees
     cur.execute("""
         alter table employees 
         add column gender varchar (10);
     """)
+    cur.execute("select * from employees;")
+    print ("Adding column 'Gender' to employees : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 12. Changing the size of column 'city'
     cur.execute("""
@@ -175,17 +242,27 @@ try:
         alter column city
         set data type varchar (150);
     """)
+    cur.execute("select * from employees;")
+    print ("Changing the size of column 'city' : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
 
     # 13. Showing top 3 highest paid employees
     cur.execute("""
         select * from employees 
         order by salary desc limit 3;
     """)
-       
+    print ("Showing top 3 highest paid employees : ")
+    for i in cur.fetchall():
+        print (i)
+    print ()
     
 
 except Exception as error:
     print (error)
+
+    
 finally :
     if cur is not None:
         cur.close() 
